241bbf985d79573627959391a8636bc873a1d475
Saeed Amiri <samiri@uni-bremen.de>
2023-10-30 09:15:24 +0100

    write input for plumed

---
+++ src/plumed_wall_input.py
@@ -0,0 +1,12 @@
+"""
+write the input for plumed to se walls for ions and nanoparticles
+"""
+
+import sys
+import pandas as pd
+
+import logger
+from colors_text import TextColor as bcolors
+
+if __name__ == "__main__":
+    pass
